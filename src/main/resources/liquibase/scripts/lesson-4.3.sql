c172c39df250b7152349498e968a4d78d078bf2a
Hybusa <alkouzne007@gmail.com>
2023-05-03 14:21:29 +0300

    4.3 done. (found a mistake, changed changesets)

--- src/main/resources/liquibase/scripts/lesson-4.3.sql
+++ src/main/resources/liquibase/scripts/lesson-4.3.sql
@@ -3,4 +3,9 @@
 -- changeset akuznetsov:1
 
 CREATE INDEX student_name_index ON student(name);
-CREATE INDEX faculty_color_index ON faculty(name,color)
+CREATE INDEX faculty_color_index ON faculty(color);
+
+--changeset akuznetsov:2
+
+DROP INDEX faculty_color_index;
+CREATE INDEX faculty_color_name ON faculty(color, name);
