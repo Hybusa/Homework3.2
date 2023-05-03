948d144debcf34e5aee4c41c0ca5fcc602bf3c0e
Hybusa <alkouzne007@gmail.com>
2023-05-03 16:14:36 +0300

    4.4 done.

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -1,5 +1,6 @@
 package ru.hogwarts.school.controller;
 
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class InfoController {
 
+    private final Environment environment;
 
+    public InfoController(Environment environment) {
+        this.environment = environment;
+    }
 
     @GetMapping("getPort")
     public ResponseEntity<String> getPort(){
-        return ResponseEntity.ok("port is: " );
+        return ResponseEntity.ok("port is: " + environment.getProperty("local.server.port") );
     }
 }
--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -20,39 +20,48 @@ public class StudentService {
     }
 
     public Student createStudent(Student student) {
+        logger.debug("Method createStudent was invoked with parameters {}", student);
         return studentRepository.save(student);
     }
 
     public Optional<Student> findStudent(Long id) {
+        logger.debug("Method findStudent was invoked with parameters {}", id);
         return studentRepository.findById(id);
     }
 
     public Student editStudent(Student student) {
+        logger.debug("Method editStudent was invoked with parameters {}", student);
         if(studentRepository.existsById(student.getId()))
             return studentRepository.save(student);
         return null;
     }
     public Collection<Student> getAllByName(String name){
+        logger.debug("Method getAllByName was invoked with parameters {}", name);
         return studentRepository.findAllByNameContainsIgnoreCase(name);
     }
 
     public Collection<Student> getAllByAgeBetween(int min, int max){
+        logger.debug("Method getAllByAgeBetween was invoked with parameters min:{} , max:{}", min,max);
         return studentRepository.findAllByAgeBetween(min, max);
     }
 
     public void deleteStudent(Long id) {
+        logger.debug("Method deleteStudent was invoked with parameters {}", id);
         studentRepository.deleteById(id);
     }
 
     public Collection<Student> getAll() {
+        logger.debug("Method getAll was invoked");
         return studentRepository.findAll();
     }
 
     public Collection<Student> getAllByAge(int age) {
+        logger.debug("Method deleteStudent was invoked with parameters {}", age);
         return studentRepository.findAllByAge(age);
     }
 
     public Faculty getFaculty(Long id) {
+        logger.debug("Method getFaculty was invoked with parameters {}", id);
         return studentRepository.findById(id).map(Student::getFaculty).orElseThrow(RuntimeException::new);
     }
 }
